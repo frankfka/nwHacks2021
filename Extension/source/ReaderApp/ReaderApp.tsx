cb69296eabb1baddb3f5e639003733f5abcd8d0e
Frank Jia <frank.jia@shopify.com>
2021-01-09 13:21:20 -0800

    Add Reader app

---
+++ Extension/source/ReaderApp/ReaderApp.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import '../styles/tailwind.css';
+
+export default function ReaderApp(): JSX.Element {
+  return (
+    <div>
+      Reader App
+    </div>
+  );
+}
---
+++ Extension/source/ReaderApp/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import ReaderApp from './ReaderApp';
+
+ReactDOM.render(<ReaderApp />, document.getElementById('reader-root'));
