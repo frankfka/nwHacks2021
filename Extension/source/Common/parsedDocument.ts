c32b8e2b77781c4d94e6817728373ea02d6d3fca
Frank Jia <frank.jia@shopify.com>
2021-01-09 13:24:42 -0800

    Add common models for communication

---
+++ Extension/source/Common/parsedDocument.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Readability from '@mozilla/readability';
+
+export type ParsedDocument = Readability.ParseResult;
---
+++ Extension/source/Common/runtimeMessage.ts
@@ -0,0 +1,17 @@
+import {ParsedDocument} from './parsedDocument';
+
+export enum RuntimeMessageType {
+  INJECT_CONTENT = 'INJECT_CONTENT',
+  PARSE_DOC = 'PARSE_DOC',
+  PARSE_MOCK_DOC = 'PARSE_MOCK_DOC',
+  PARSE_DOC_SUCCESS = 'PARSE_DOC_SUCCESS',
+  GET_PARSED_DOC = 'GET_PARSED_DOC',
+}
+
+export interface RuntimeMessage {
+  type: RuntimeMessageType;
+}
+
+export interface ParseDocumentSuccessMessage extends RuntimeMessage {
+  parsed: ParsedDocument;
+}
